d616154418ce1b8eb49aab45577c08bbd322861b
ren <84360463+rencryptofish@users.noreply.github.com>
2023-04-10 13:07:06 -0400

    add head and description

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,16 @@
-import 'tailwindcss/tailwind.css'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>EIPs.wtf</title>
+        <meta name="description" content="EIPs.wtf provides extra information for Ethereum Improvement Proposals (EIPs), including their status, implementation details, related pull requests, and community feedback. This helps Ethereum developers and community members better understand EIPs and facilitates the implementation of new proposals." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
