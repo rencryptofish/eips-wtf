dfdd3f6ac2024c82d67f101dd6382a6de6580fd7
ren <84360463+rencryptofish@users.noreply.github.com>
2023-04-11 01:30:08 -0400

    add ogp

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,15 +3,29 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+const SITE_TITLE = 'eips.wtf';
+const SITE_DESCRIPTION = 'Enhanced stats for Ethereum Improvement Proposals (EIPs)';
+const SITE_DOMAIN = 'eips.wtf';
+const SITE = 'https://' + SITE_DOMAIN;
+const SITE_BANNER = '/ogp_banner.png';
+const SITE_TWITTER = '@0xren_cf';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>EIPs.wtf</title>
-        <meta
-          name='description'
-          content='EIPs.wtf provides extra information for Ethereum Improvement Proposals (EIPs), including their status, implementation details, related pull requests, and community feedback. This helps Ethereum developers and community members better understand EIPs and facilitates the implementation of new proposals.'
-        />
+        <title>{SITE_TITLE}</title>
+        <meta name='description' content={SITE_DESCRIPTION} key='DESCRIPTION' />
+
+        {/* Twitter Meta Tags */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:domain' content={SITE_DOMAIN} />
+        <meta name='twitter:url' content={SITE} />
+        <meta name='twitter:title' content={SITE_TITLE} key='TWITTER_TITLE' />
+        <meta name='twitter:site' content={SITE_TWITTER} />
+        <meta name='twitter:creator' content={SITE_TWITTER} />
+        <meta name='twitter:description' content={SITE_DESCRIPTION} key='TWITTER_DESCRIPTION' />
+        <meta name='twitter:image' content={SITE + SITE_BANNER} key='TWITTER_IMAGE' />
       </Head>
       <Component {...pageProps} />
     </>
